1ce18cfe4e7f88d7de5386da4960eb3a7ed5f3eb
Asya <web.zaslavskaya@gmail.com>
2022-12-17 04:47:48 +0200

    banner+

--- js/scripts.js
+++ js/scripts.js
@@ -306,3 +306,19 @@ tabs.forEach(tab => {
     target.classList.add('active')
   })
 });
+
+
+$('.project_slider').slick({
+	slidesToShow: 3,
+	slidesToScroll: 3,
+	arrows: false,
+	 responsive: [
+	  {
+		breakpoint: 1200,
+		settings: {
+		  slidesToShow: 2,
+		  slidesToScroll: 2,
+		  arrows: false
+		}
+	  }]
+});
